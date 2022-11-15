26a0a205527af1116ce7593f95167da1593d3063
feerinka <114405337+feerinka@users.noreply.github.com>
2022-11-15 09:00:51 +0200

    karogs
    new

---
+++ 15.11.py
@@ -0,0 +1,26 @@
+from turtle import*
+a=Turtle()
+a.shape("turtle")
+a.pencolor("black")
+a.fillcolor("dark red")
+a.pensize(4)
+
+a.begin_fill()
+
+a.goto(0,250)
+a.goto(500,250)
+a.goto(500,0)
+a.goto(0,0)
+a.goto(0,100)
+a.goto(500,100)
+a.goto(500,150)
+a.goto(0,150)
+
+a.end_fill()
+
+    
+
+
+
+
+
